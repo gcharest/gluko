da841564df13a00e9d1439eb662059bba9f57af4
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-17 21:48:01 -0400

    Add temp Calculator component

---
+++ src/components/Calculator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <form id="calculator">
+      <input v-model="facteur" placeholder="facteur" />
+    </form>
+    <p>{{ facteur }}</p>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Options } from "vue-class-component";
+@Options({
+  data() {
+    return {
+      facteur: ""
+    };
+  },
+  props: {
+    msg: String
+  },
+  components: {}
+})
+export default class Calculator extends Vue {
+  msg!: string;
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Calculator />
   </div>
 </template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Calculator from "@/components/Calculator.vue";
+
+@Options({
+  components: {
+    Calculator
+  }
+})
+export default class About extends Vue {}
+</script>
