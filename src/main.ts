a49b1cf5d51eea1ce39e7fdf6d474d6c7d17ca8e
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-19 21:44:35 -0400

    Update store path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import router from "@/router";
+import { store } from "@/store/index";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
