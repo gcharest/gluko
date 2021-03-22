d81230070cc342aad6c388967b967a52b53d4427
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-21 20:55:46 -0400

    Fix lint

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>Facteur glucidique</h1>
-    <p>Cette page contiendra l'outil de calcul des facteurs glucidiques des recettes.</p>
+    <p>
+      Cette page contiendra l'outil de calcul des facteurs glucidiques des
+      recettes.
+    </p>
   </div>
 </template>
