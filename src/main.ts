4ce30977e584af2922c7ea6c8868601189c95701
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-21 20:48:14 -0400

    Create i18n

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+const i18n = createI18n({});
+
 createApp(App)
   .use(store)
   .use(router)
+  .use(i18n)
   .mount("#app");
