79bc812a5755a227dc08dc89406255e4564d5a1d
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-23 23:34:01 -0400

    Add base meal vue

--- src/components/Meal.vue
+++ src/components/Meal.vue
@@ -1,20 +1,42 @@
 <template>
   <div>
-    <table>
-      <tr>
-        <th>
-          Nom
-        </th>
-        <th>
-          Facteur
-        </th>
-        <th>
-          Quantité
-        </th>
-        <th>
-          Sous-total
-        </th>
-      </tr>
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th>
+            {{ $t("meal.tableHeader.nutrient") }}
+          </th>
+          <th>
+            {{ $t("meal.tableHeader.factor") }}
+          </th>
+          <th>
+            {{ $t("meal.tableHeader.quantity") }}
+          </th>
+          <th>
+            {{ $t("meal.tableHeader.subtotal") }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>123</td>
+          <td>125</td>
+          <td>456</td>
+          <td>4465</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
+<script>
+import { computed, defineComponent } from "@vue/runtime-core";
+import { useStore } from "@/store/index";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    const nutrients = computed(() => store.state);
+    return {};
+  }
+});
+</script>
