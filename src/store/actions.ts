b6acc6825e8e0f9cac4e0e0d405846e8c5d86c03
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-23 23:35:19 -0400

    Add first actions to actions.ts

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,3 +1,26 @@
 // store/actions.ts
 import { ActionContext, ActionTree } from "vuex";
-import { Mutations, mutations } from "@/store/mutations";
+import { Mutations, MutationType } from "@/store/mutations";
+import { State } from "@/store/state";
+
+export enum ActionTypes {
+  GetProfileItems = "GET_PROFILE_ITEMS"
+}
+
+type ActionAugments = Omit<ActionContext<State, State>, "commit"> & {
+  commit<K extends keyof Mutations>(
+    key: K,
+    payload: Parameters<Mutations[K]>[1]
+  ): ReturnType<Mutations[K]>;
+};
+
+export type Actions = {
+  [ActionTypes.GetProfileItems](context: ActionAugments): void;
+};
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+export const actions: ActionTree<State, State> & Actions = {
+  async [ActionTypes.GetProfileItems]({ commit }) {
+    commit(MutationType.SetLoading, true);
+  }
+};
