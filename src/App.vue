08305efb339cf4d7c277cf361cf008e3fc1a16ed
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-23 23:33:03 -0400

    Add main navigation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <div id="nav">
-    <router-link to="/">{{ $t("navigation.home") }}</router-link> |
-    <router-link to="/about">{{ $t("navigation.recipe") }} </router-link>
+  <div class="skippy visually-hidden-focusable overflow-hidden">
+    <div class="container-xl">
+      <a class="d-inline-flex p-2 m-1" href="#content">
+        {{ $t("navigation.skipToContent") }}
+      </a>
+    </div>
   </div>
-  <router-view />
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand visually-hidden-focusable" href="#">{{
+        $t("navigation.mainNavigation")
+      }}</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link" aria-current="page">{{
+              $t("navigation.home")
+            }}</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link"
+              >{{ $t("navigation.recipe") }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <main>
+    <router-view />
+  </main>
 </template>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
