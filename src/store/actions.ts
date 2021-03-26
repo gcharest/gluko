6b9a892c37bea596ab69f66d0606239c27d01f12
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-26 16:57:27 -0400

    Add actions

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -2,9 +2,11 @@
 import { ActionContext, ActionTree } from "vuex";
 import { Mutations, MutationType } from "@/store/mutations";
 import { State } from "@/store/state";
+import defaultData from "@/assets/default.json";
 
 export enum ActionTypes {
-  GetProfileItems = "GET_PROFILE_ITEMS"
+  GetProfileItems = "GET_PROFILE_ITEMS",
+  SetProfileItems = "SET_PROFILE_ITEMS"
 }
 
 type ActionAugments = Omit<ActionContext<State, State>, "commit"> & {
@@ -16,11 +18,16 @@ type ActionAugments = Omit<ActionContext<State, State>, "commit"> & {
 
 export type Actions = {
   [ActionTypes.GetProfileItems](context: ActionAugments): void;
+  [ActionTypes.SetProfileItems](context: ActionAugments): void;
 };
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 export const actions: ActionTree<State, State> & Actions = {
   async [ActionTypes.GetProfileItems]({ commit }) {
     commit(MutationType.SetLoading, true);
+    commit(MutationType.SetProfiles, defaultData.profiles);
+    commit(MutationType.SetLoading, false);
+  },
+  async [ActionTypes.SetProfileItems]({ commit }) {
+    commit(MutationType.SetProfiles, defaultData.profiles);
   }
 };
