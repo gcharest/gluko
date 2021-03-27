e84441a292a24eee1fae8b0b4966a6ec06492bd8
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-03-27 00:08:14 -0400

    Comment out marked

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "@/router";
 import { store } from "@/store/index";
-//import bootstrap from "bootstrap";
-//import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap";
+// import "marked";
+// import "bootstrap/dist/css/bootstrap.css";
 import en from "@/locale/en-ca.json";
 import fr from "@/locale/fr-ca.json";
 
