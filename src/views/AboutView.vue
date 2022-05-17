fbe27c09bf107ea32aaa0be4c97462d7775fe1b0
Guillaume Charest <guillaume.w.charest@gmail.com>
2022-05-16 20:45:39 -0400

    Fix text class for dark mode

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="about container">
-    <h1>{{ $t("about") }}</h1>
-    <p>
+    <h1 class="text-light">{{ $t("about") }}</h1>
+    <p class="text-light">
       Cette application a pour objectif de faciliter le calcul des glucides.
     </p>
   </main>
