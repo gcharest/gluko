cfb4ceacaf253378ea8470afbb28532fc916b568
Guillaume Charest <guillaume.w.charest@gmail.com>
2023-03-30 13:10:37 -0400

    feat: add userSession store

---
+++ src/stores/session.ts
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+import { useLocalStorage } from "@vueuse/core";
+
+export type userSession = {
+  dismissedExperimentNotice: boolean;
+};
+
+export const userSessionStore = defineStore({
+  id: "userSession",
+  state: () => ({
+    userSession: useLocalStorage("userSession", {} as userSession),
+  }),
+  getters: {
+    getUserSession(): userSession {
+      return this.userSession;
+    },
+  },
+  actions: {
+    initialize() {
+      this.userSession = {
+        dismissedExperimentNotice: false,
+      };
+    },
+    reset() {
+      this.userSession = {
+        dismissedExperimentNotice: false,
+      };
+    },
+  },
+});
