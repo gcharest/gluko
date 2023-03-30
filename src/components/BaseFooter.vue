9a4d7eda832db85b83fd239e53f35fe8f0a71e79
Guillaume Charest <guillaume.w.charest@gmail.com>
2023-03-30 13:09:37 -0400

    feat: add notice toggle button

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,7 +1,56 @@
+<script setup lang="ts"></script>
 <template>
   <footer class="footer mt-auto py-3 bg-dark">
     <div class="container">
-      <span class="text-light">© Guillaume Charest 2021-2022</span>
+      <div class="row">
+        <div class="col">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-toggle="modal"
+            data-bs-target="#experiment"
+          >
+            {{ $t("Display Notice") }}
+          </button>
+          <Teleport to="body">
+            <div
+              class="modal modal-fullscreen-md-down fade"
+              id="experiment"
+              tabindex="-1"
+              aria-hidden="false"
+            >
+              <div class="modal-dialog bg-light">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="experimentLabel">
+                    {{ $t("Notice") }}
+                  </h5>
+                </div>
+                <div class="modal-body">
+                  <div class="row">
+                    <div class="col">
+                      <p>{{ $t("Experiment Notice") }}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-dismiss="modal"
+                  >
+                    {{ $t("Close") }}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Teleport>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <span class="text-light">Gluko © Guillaume Charest 2021-2022</span>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
